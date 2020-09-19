9b7f353af2677eca9487931f51fc9cf265e412f5
xiluna <xilaluna2@gmail.com>
2020-09-19 00:11:35 -0700

    cart fully functional

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,6 +5,29 @@ const itemList = document.getElementById("item-list");
 const cartQty = document.getElementById("cart-qty");
 const cartTotal = document.getElementById("cart-total");
 
+const cart = [];
+
+itemList.onchange = function (e) {
+  if (e.target && e.target.classList.contains("update")) {
+    const name = e.target.dataset.name;
+    const qty = parseInt(e.target.value);
+    updateCart(name, qty);
+  }
+};
+
+itemList.onclick = function (e) {
+  if (e.target && e.target.classList.contains("remove")) {
+    const name = e.target.dataset.name;
+    removeItem(name);
+  } else if (e.target && e.target.classList.contains("add-one")) {
+    const name = e.target.dataset.name;
+    addItem(name);
+  } else if (e.target && e.target.classList.contains("remove-one")) {
+    const name = e.target.dataset.name;
+    removeItem(name, 1);
+  }
+};
+
 for (let i = 0; i < data.length; ++i) {
   let newDiv = document.createElement("div");
   newDiv.className = "item";
@@ -44,8 +67,6 @@ all_items_button.forEach((elt) =>
   })
 );
 
-const cart = [];
-
 function addItem(name, price) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].name === name) {
@@ -64,8 +85,13 @@ function showItems() {
   let itemStr = "";
   for (let i = 0; i < cart.length; i++) {
     const { name, price, qty } = cart[i];
-    itemStr += `<li>${name} $${price} x ${qty} = ${qty * price}
-      </li>`;
+    itemStr += `<li>
+      ${name} $${price} x ${qty} = ${qty * price} 
+      <button class="remove" data-name="${name}">Remove</button>
+      <button class="add-one" data-name="${name}">+</button>
+      <button class="remove-one" data-name="${name}">-</button>
+      <input class="update" type="number" data-name="${name}">
+    </li>`;
   }
   itemList.innerHTML = itemStr;
 
@@ -98,6 +124,22 @@ function removeItem(name, qty = 0) {
       if (cart[i].qty < 1 || qty === 0) {
         cart.splice(i, 1);
       }
+      showItems();
+      return;
+    }
+  }
+}
+
+function updateCart(name, qty) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      if (qty < 1) {
+        removeItem(name);
+        return;
+      }
+      cart[i].qty = qty;
+      showItems();
+
       return;
     }
   }
