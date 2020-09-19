cdefb8cd4b3cf6cef13caa7b65ff81925d5ec49d
xiluna <xilaluna2@gmail.com>
2020-09-18 21:29:20 -0700

    displayed cart items on console

--- resources/js/script.js
+++ resources/js/script.js
@@ -34,13 +34,41 @@ for (let i = 0; i < data.length; ++i) {
 const cart = [];
 
 function addItem(name, price) {
-  const item = { name: name, price: price, qty: 1 };
-
+  for (let = i; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      cart[i].qty++;
+      return;
+    }
+  }
+  const item = { name, price, qty: 1 };
   cart.push(item);
 }
 
 function showItems() {
-  console.log(`You have ${cart.length} items in your cart`);
+  const qty = getQty();
+
+  console.log(`You have ${qty} items in your cart`);
+  for (let i = 0; i < cart.length; i++) {
+    console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`);
+  }
+  const total = getTotal();
+  console.log(`Total in cart: $${total}`);
+}
+
+function getQty() {
+  let qty = 0;
+  for (let i = 0; i < cart.length; i++) {
+    qty += cart[i].qty;
+  }
+  return qty;
+}
+
+function getTotal() {
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price * cart[i].qty;
+  }
+  return total.toFixed(2);
 }
 
 addItem("Apple", 0.99);
